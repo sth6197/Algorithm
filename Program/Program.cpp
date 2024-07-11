91d95a8ddfe6bcb94c23aaeae4e250341ea91e74
STH <sth6197@gmail.com>
2024-07-11 19:47:47 +0900

    (2024.07.11) 브루트 포스

---
+++ Program/Program.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool number(int n)
+{
+	if (n <= 1)
+	{
+		return false;
+	}
+
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int main()
+{
+	cout << number(5) << endl;
+	cout << number(10) << endl;
+	cout << number(7) << endl;
+	cout << number(3) << endl;
+	cout << number(2) << endl;
+	
+
+
+	return 0;
+}
