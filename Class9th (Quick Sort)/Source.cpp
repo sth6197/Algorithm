4f92a67530f3d7ba40e6309a121898c6352b27ac
STH <sth6197@gmail.com>
2024-07-18 19:46:29 +0900

    (2024.07.18) 병합 정렬

---
+++ Class9th (Quick Sort)/Source.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 8
+
+void quick(int list[], int start, int end)
+{
+    if (start >= end)
+    {
+        return;
+    }
+
+    // pivot 변수의 값을 설정합니다.
+    int pivot = start;
+
+    // left 변수의 값을 설정합니다.
+    int left = start + 1;
+
+    // right 변수의 값을 설정합니다.
+    int right = end;
+
+    int swap;
+
+    // left 가 right 보다 크거나 같을 때까지 반복합니다.
+    while (left <= right)
+    {
+        // left가 end보다 작거나 같고 list[left]가 list[pivot] 보다 작거나 같을 때까지
+        // 반복합니다.
+        while (left <= end && list[left] <= list[pivot])
+        {
+            left++;
+        }
+
+        // right가 start보다 크고 list[right]가 list[pivot]보다 크거나 같을 때까지
+        // 반복합니다.
+        while (right > start && list[right] >= list[pivot])
+        {
+            right--;
+        }
+
+        if (left > right)
+        {
+            swap = list[right];
+            list[right] = list[pivot];
+            list[pivot] = swap;
+        }
+        else
+        {
+            swap = list[left];
+            list[left] = list[right];
+            list[right] = swap;
+        }
+    }
+
+    // 피벗을 기준으로 나누어진 두 배열에 대해 재귀적으로 퀵 정렬을 호출합니다.
+    quick(list, start, right - 1);
+    quick(list, right + 1, end);
+
+    return;
+}
+
+
+int main()
+{
+#pragma region 퀵 정렬
+    // 기준점을 획득한 다음 해당 기준점을 기준으로
+    // 배열을 나누고 한 쪽에는 기준점보다 작은 값들이
+    // 위치하고 다른 한 쪽에는 기준점보다 큰 값들이 위치하도록
+    // 정렬하는 알고리즘입니다.
+
+    // 나누어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여
+    // 모든 배열이 기본 배열이 될 때까지 반복하면서 정렬합니다.
+
+    int list[SIZE] = { 2, 6, 8, 10, 4, 3, 9, 5 };
+
+    quick(list, 0, SIZE - 1);
+
+    for (const int& element : list)
+    {
+        cout << element << " ";
+    }
+#pragma endregion
+
+
+
+
+
+    return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -4,70 +4,66 @@ using namespace std;
 
 #define SIZE 8
 
-int quick(int list[], int left, int right)
+void MergeSort(int list[], int start, int end)
 {
-    if (left >= right)
+    if (start < end)
     {
-        return list[left];
+        int mid = (start + end) / 2;
+
+        MergeSort(list, start, mid);
+        MergeSort(list, mid + 1, end);
     }
+}
 
-    int pivot = left;
-    int qleft = left + 1;
-    int qright = right;
-
-    int swap;
-
-    while (qleft <= qright)
+void Merge(int list[], int start, int mid, int end)
+{
+    int left = start;
+    int right = end;
+    int pivot = mid + 1;
+    
+    while (left <= mid && pivot <= end)
     {
-        while (qleft <= right && list[qleft] <= list[pivot])
+        if (list[left] <= list[pivot])
         {
-            qleft++;
-        }
-        
-        while (qright > left && list[qright] >= list[pivot])
-        {
-            qright--;
-        }
-
-        if(qleft > qright)
-        {
-            swap = list[qright];
-            list[qright] = list[pivot];
-            list[pivot] = swap;
+            list[right++] = list[left++];
         }
         else
         {
-            swap = list[qleft];
-            list[qleft] = list[qright];
-            list[qright] = swap;
+            list[right++] = list[pivot++];
         }
+
     }
 
-    quick(list, left, qright - 1);
-    quick(list, qright + 1, right);
+    
+
+
+
+
 }
 
 
 int main()
 {
-#pragma region 퀵 정렬
-    // 기준점을 획득한 다음 해당 기준점을 기준으로
-    // 배열을 나누고 한 쪽에는 기준점보다 작은 값들이
-    // 위치하고 다른 한 쪽에는 기준점보다 큰 값들이 위치하도록
-    // 정렬하는 알고리즘입니다.
+#pragma region 병합 정렬
+    // 하나의 리스트를 두 개의 균등한 크기로 분할하고 분할된
+    // 부분 리스트를 정렬한 다음, 두 개의 정렬된 부분 리스트를
+    // 합하여 전체가 정렬된 리스트가 되게 하는 방법입니다.
 
-    // 나누어진 하위 배열에 대해 재귀적으로 퀵 정렬을 호출하여
-    // 모든 배열이 기본 배열이 될 때까지 반복하면서 정렬합니다.
+    // 1. 리스트의 길이가 0 또는 1이면 이미 정렬된 것으로 봅니다.
 
-    int list[SIZE] = { 2, 6, 8, 10, 3, 4, 5, 9 };
+    // 2. 그렇지 않은 경우
 
-    quick(list, 0, SIZE - 1);
+    // 2-1. 정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의 두 부분 리스트로
+    //      나눕니다.
 
-    for(int i = 0; i < SIZE; i++)
-    {
-        cout << list[i] << ' ';
-    }
-    
+    // 2-2. 각 부분 리스트를 재귀적으로 병합 정렬을 이용하여 정렬합니다.
+
+    // 2-3. 두 부분 리스트를 다시 하나의 정렬된 리스트로 병합합니다.
+
+    int list[SIZE] = { 3, 5, 2, 7, 4, 1, 8, 6 };
+
+    MergeSort(list, 0, SIZE - 1);
+
 
 #pragma endregion
 
