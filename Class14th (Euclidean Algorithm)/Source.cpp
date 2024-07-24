19bf8b4b003700a5f5ebf9f35002b77e36855360
STH <sth6197@gmail.com>
2024-07-24 19:51:58 +0900

    (2024.07.24) 백트래킹

---
+++ Class14th (Euclidean Algorithm)/Source.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int Euclid(int x, int y)
+{
+    if (y == 0)
+    {
+        return x;
+    }
+    else
+    {
+        return Euclid(y, x % y);
+    }
+}
+
+int main()
+{
+#pragma region 유클리드 호제법
+    // 2개의 자연수 또는 정식의 최대공약수를 구하는 방식으로,
+    // 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
+
+    int x = 78696;
+    int y = 19332;
+
+    cout << "두 수의 최대 공약수 : " << Euclid(max(x, y), min(x, y)) << endl;
+#pragma endregion
+
+
+    return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,24 +2,45 @@
 
 using namespace std;
 
+void hanoi(int n, char from, char by, char to)
+{
+    // 개수 : n
+    // 출발 : from
+    // 경유 : by
+    // 도착 : to
+
+    if (n == 1)
+    {
+        cout << from << " → " << to << endl;
+    }
+    else
+    {
+        hanoi(n - 1, from, to, by);
+        
+        cout << from << " → " << to << endl;
+
+        hanoi(n - 1, by, from, to);
+    }
+}
 
 int main()
 {
-    int a = 0;
-    int b = 0;
+#pragma region 백트래킹
+    // 해를 찾아가는 도중에 지금의 경로가 해가 될 것 같지 않으면
+    // 더 이상 깊이 들어가지 않고, 이전 단계로 다시 돌아가는 알고리즘입니다.
 
-    int c = 0;
+    // 백트래킹의 유망성 판단
+    // 해가 될 만한지 판단한 후에 유망하지 않다고 결정되면, 
+    // 그 노드의 이전 노드로 돌아가 다음 자식 노드로 이동합니다.
 
-    cin >> a >> b;
-
-    while (b != 0)
-    {
-        c = a % b;
-        a = b;
-        b = c;
-    }
+    // 해가 될 만한 가능성이 있으면 유망하다. (Promising)
+    // 유망하지 않은 노드에 가지 않는 것      (Pruning)
     
-    cout << "최대공약수 : " << a, b;
+    hanoi(3, 'A', 'B', 'C');
+
+#pragma endregion
+
+
 
 
     return 0;
