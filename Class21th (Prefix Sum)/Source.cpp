ff5b5984e414c5051ee063fafa506413ba77d329
STH <sth6197@gmail.com>
2024-08-01 19:46:15 +0900

    (2024.08.01) 다익스트라 알고리즘

---
+++ Class21th (Prefix Sum)/Source.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 5
+
+int main()
+{
+#pragma region 누적합
+    // 특정한 배열이 있을 때, 배열까지의 합을 나타내는 것입니다.
+
+    int list[SIZE] = { 1, 2, 3, 4, 5 };
+    int sum[SIZE] = { 0, 0, 0, 0, 0 };
+
+    sum[0] = list[0];
+
+    for (int i = 1; i < SIZE; i++)
+    {
+        sum[i] = sum[i - 1] + list[i];
+    }
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << sum[i] << " ";
+    }
+#pragma endregion
+
+
+
+    return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,29 +2,107 @@
 
 using namespace std;
 
-#define SIZE 5
+#define SIZE 6
+#define INF 10000000
+
+class Graph
+{
+private:
+    int weight[SIZE][SIZE] = 
+    {
+      { 0, 2, 5, 3, INF, INF },
+      { 2, 0, 7, INF, INF, 9 },
+      { 5, 7, 0, 1, 5, 2 },
+      { 3, INF, 1, 0, 7, INF },
+      { INF, INF, 2, 7, 0, 2 },
+      { INF, 9, 5, INF, 2, 0 } 
+    };
+
+    int distance[SIZE];
+    bool visited[SIZE];
+
+
+public:
+    Graph()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            visited[i] = 0;
+        }
+    }
+    
+    int SmallNode()
+    {
+        int min = INF;
+        int minval = 0;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            if (distance[i] < min && visited[i] == false)
+            {
+                min = distance[i];
+                minval = i;
+            }
+        }
+        return minval;
+    }
+
+    void Dijkstra(int start)
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            distance[i] = weight[start][i];
+        }
+
+        // 방문처리
+        visited[start] = true;
+        
+        for (int i = 0; i < SIZE - 1; i++)
+        {
+           i = SmallNode();
+        }
+
+        // 방문처리
+        visited[start] = true;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            
+
+        }
+
+    }
+
+};
 
 int main()
 {
-#pragma region 누적합
-    // 특정한 배열이 있을 때, 배열까지의 합을 나타내는 것입니다.
+#pragma region 다익스트라 알고리즘
+    // 시작점으로부터 모든 노드까지의 최소 거리를 구해주는
+    // 알고리즘입니다.
 
-    int list[SIZE] = { 1, 2, 3, 4, 5 };
-    int sum[SIZE] = { 0, 0, 0, 0, 0 };
+    // 1. distance 배열에 weight[시작점 노드]의 값들로 초기화합니다.
 
-    sum[0] = list[0];
+    // 2. 시작점을 방문 처리합니다.
 
-    for (int i = 1; i < SIZE; i++)
-    {
-        sum[i] = sum[i - 1] + list[i];
-    }
+    // 3. distance 배열에서 최소 비용 노드를 찾고 방문 처리합니다.
+    //    단, 이미 방문한 노드는 제외합니다.
 
-    for (int i = 0; i < SIZE; i++)
-    {
-        cout << sum[i] << " ";
-    }
+    // 4. 최소 비용 노드를 거쳐갈 지 고민해서 distance 배열을 갱신합니다.
+    //    단, 이미 방문한 노드는 제외합니다.
+
+    // 5. 모든 노드를 방문할 때까지 3번 ~ 4번을 반복합니다.
+
+    // 방문하지 않은 노드 중에서 가장 작은 distance를 가진 노드를 방문하고,
+    // 그 노드와 연결된 다른 노드까지의 거리를 계산합니다.
+
+    Graph graph;
+    
+    graph.Dijkstra(1);
+
 
 #pragma endregion
+
 
 
 
